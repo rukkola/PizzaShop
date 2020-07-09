2cfe4390395874d5200eecdc92656d3f85a2aac1
serj <seregaproger@yandex.ru>
2020-07-09 22:40:25 +0300

    created migration

--- app.rb
+++ app.rb
@@ -2,6 +2,14 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+#set :datdbase, "sqlite3:pizzashop.db"
+set :database, {adapter: "sqlite3", database: "pizzashop.db"}
+
+class Product < ActiveRecord::Base
+
+end
 
 get '/' do
 	erb :index
