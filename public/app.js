1b3331abd5b1cfd9c976e03d1c67a0e2dea5ef2b
serj <seregaproger@yandex.ru>
2020-07-10 19:26:29 +0300

    update add to  card

--- public/app.js
+++ public/app.js
@@ -7,3 +7,13 @@ function som()
 		alert(x);
 
 	}
+
+function add_to_cart(id)
+	{	
+		var key = 'product_' + id;
+
+		var x = window.localStorage.getItem(key);
+		x = x * 1 + 1;
+		window.localStorage.setItem(key, x);
+		alert('Yoy add pizza' + id);
+	}
