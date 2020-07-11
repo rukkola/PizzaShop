9665ac49945e90e48da54bc112fa0718e0b28b64
serj <seregaproger@yandex.ru>
2020-07-11 14:21:37 +0300

    update orders input field

--- public/app.js
+++ public/app.js
@@ -15,5 +15,46 @@ function add_to_cart(id)
 		var x = window.localStorage.getItem(key);
 		x = x * 1 + 1;
 		window.localStorage.setItem(key, x);
-		alert('Yoy add pizza' + id);
+		//alert('Items in your cart: ' + cart_get_number());
+		update_orders_input();
 	}
+
+function update_orders_input()
+{
+	var orders = cart_get_orders()
+	$('#orders_input').val(orders); // #обращаемся по id, если .то атрибут клас // синтаксис jqwery
+}
+
+function cart_get_number()
+{	
+	var cnt = 0;
+
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i);
+		var value = window.localStorage.getItem(key);
+		
+		if(key.indexOf('product_') == 0)
+		{
+			cnt = cnt + value * 1;
+		}
+	}
+	return cnt;
+}
+
+function cart_get_orders()
+{	
+	var orders = '';
+
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i);
+		var value = window.localStorage.getItem(key);
+		
+		if(key.indexOf('product_') == 0)
+		{
+			orders = orders + key + '=' + value + ',';
+		}
+	}
+	return orders;
+}
